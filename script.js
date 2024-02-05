6be1e065eae3f4451a3e9613b4230ec663585516
neimaaman <neimaaman1234@gmail.com>
2024-02-05 14:40:20 -0500

    added append child and div

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // in js get the HTML element that we want to watch for an event on...
 
-const productForm = document.querySelector('product-form');//document object refers to the whole page of the website we r working on 
+const productForm = document.querySelector('#product-form');//document object refers to the whole page of the website we r working on 
 console.log(productForm);
 
 // to get specific element form html to js
@@ -10,42 +10,72 @@ console.log(productForm);
 //2. in js add some EventListners that watches for form submit
 // i want to watch for two thing 1 when the user submit the form 2 after the submit
 // 2a. data type....string
-productForm.addEventListener('submit', );
 // 3b. JS, >> function <<
+
 let productCatalog = [];
 
-const preview = function( event ) {
+const preview = function (event) {
     event.preventDefault(); // stop the default action!
     console.log(event);
 
 
-// in js when a form submit happens, get all the values form the input and add to an array
-const nameEl = document.querySelector('#product-name');
-const priceEl = document.querySelector('#product-price');
-const checkedEl = document.querySelector('#product-isprime');
+    // in js when a form submit happens, get all the values form the input and add to an array
+    const nameEl = document.querySelector('#product-name');
+    const priceEl = document.querySelector('#product-price');
+    const currencyEl = document.querySelector('#product-currency');
+    const amountEl = document.querySelector('#product-amount')
+    const checkedEl = document.querySelector('#product-isprime');
+        console.log(nameEl)
+        console.log(priceEl)
+        console.log(currencyEl)
+        console.log(amountEl)
+        console.log(checkedEl)
+    const newProduct = {
+        productName: nameEl.value,
+        productPrice: Number(priceEl.value),
+        productCurrency: currencyEl.value,
+        productAmount: amountEl.value,
+        isPrime: checkedEl.checked,
+    };
 
 
-const newProduct = {
-    prodctName: nameEl.value,
-    productPrice: Number(priceEl.value),
-    isPrime: checkedEl.checked,
+    productCatalog.push(newProduct);
+    console.log(productCatalog);
+
+    for (let product of productCatalog) {
+        console.log(
+            `
+         product Name: ${product['productName']}
+         product price: $${product['productPrice']}
+         product currency: ${product['productCurrency']}
+         product amount: ${product['productAmount']}
+        `
+        );
+        if (product['isPrime']) {
+            console.log('best buy')
+        }
+
+    }
+
+    displayProducts()
+
 };
-
-
-productCatalog.push(newProduct);
-console.log(productCatalog);
+productForm.addEventListener('submit', preview);
+console.log(productForm)
+function displayProducts() {
+    const bodyElement = document.querySelector('#product-result')
 
 for (let product of productCatalog) {
-    console.log(
-        `
-        product Name: ${product['productName']}
-        product price: $${product['productPrice']}
-        `
-    );
 
-    if(product['isPrime']) {
-        console.log('something');
-    }
+    
+    const newText = document.createElement("div");
+    newText.classList.add("h-screen", "flex", "justify-center", "items-center");
+    newText.innerHTML = `<p> product Name: ${product['productName']} </p>
+                         <p> product price: $${product['productPrice']} </P>
+                         <p> product currency: ${product['productCurrency']} </p>
+                        <p> product amount: ${product['productAmount']} </p>
+                        `
+    bodyElement.appendChild(newText)
+    
 }
 };
-productForm.addEventListener( 'submit', preview);
