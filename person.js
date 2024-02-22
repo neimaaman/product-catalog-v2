ff6453859084136a83aa237cc1a5f929315f7815
neimaaman <neimaaman1234@gmail.com>
2024-02-22 16:36:12 -0500

    fixed issue with the method person class

--- person.js
+++ person.js
@@ -1,43 +1,27 @@
-class person{
-    constructor(fName,lName,age,email){
+class Person {
+    constructor(fName, lName, age, email) {
   
       this.fName = fName;
       this.lName = lName;
       this.age = age;
       this.email = email;
-      this.address = []
+      this.address = address
   
     }
-  getFullName(){
-  
-    return this.fName + this.lName;
-  }
-  
-  getEmail(){
-  
-    return this.email
-  }
     
-  fullInfo(){
-    this.address.push(
-      this.getFullName(),
-      this.getEmail(),
-      this.age
   
-    )
-    // console.log(this.address)
+  
+    fullInfo() { 
+      return{
+        fullName: this.fName +this.lName,
+        age: this.age,
+        email: this.email,
+        address: this.address
+  
+    
+      }
   
     }
-  
+   
   }
-  
-  const personOne = new person('sam','romm', '23', 'samromm@gmail.com');
-  personOne.fullInfo();
-  console.log(personOne)
-  
-  const personTwo = new person('selem', 'crane', '23', 'selemcrane@gmail.com');
-  personTwo.fullInfo();
-  console.log(personTwo)
-  // const fullName = personTwo.getFullName();
-  // console.log(fullName)
   
