6239932d32d80861b522a81f25d66f59a9c38f6d
neimaaman <neimaaman1234@gmail.com>
2024-02-22 14:14:42 -0500

    fixed my product class

--- product.js
+++ product.js
@@ -6,53 +6,45 @@ class product {
       this.productAmount = productAmount;
       this.productList = [] //initialize productList as an empty array
     }
+    
+  
+    showName() {
+      this.productName
   
   
+      return this.productName;
   
-    addProduct() {
-      this.productList.push(
-        {
-            
-          productName: this.productName,
-          productPrice: this.productPrice,
-          productCurrency:this. productCurrency,
-          productAmount: this.productAmount,
-        }
-        
-      )
-      return this.productList
     }
-
   
-    getAllProduct() {
-        this.productList;
-        
-    
-        return this.productList ;
-    
-      }
-
-
+  showProductDetail() {
+    this.productList.push(
+      this.productName,
+      this.productPrice,
+      this.productAmount,
+      this.productCurrency
+    )
+  
+    return this.productList
+  }
   }
   
-  // create an instance of the product class
+  // // create an instance of the product class
   const productOne = new product('sandales', '110', 'USD', '23');
   
-  // call getAllProduct to add productdetails to the productList
-  productOne.addProduct()
+  // // call showName to add productdetails to the productList
+  productOne.showName()
+  productOne.showProductDetail()
   console.log(productOne)
   console.log(productOne.productList)
   
   const productTwo = new product('headphone', '111', 'EUR', '234')
-  productTwo.addProduct()
+  productTwo.showName()
+  productTwo.showProductDetail()
+  console.log(productTwo)
   console.log(productTwo.productList)
-
-
-
-
-
-
-
+  
+  
+  
 
 
 
