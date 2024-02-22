f1305041c3eb14de795df9a143a07631d8531b27
neimaaman <neimaaman1234@gmail.com>
2024-02-22 14:41:42 -0500

    removed instances of the product class

--- product.js
+++ product.js
@@ -28,20 +28,7 @@ class product {
   }
   }
   
-  // // create an instance of the product class
-  const productOne = new product('sandales', '110', 'USD', '23');
   
-  // // call showName to add productdetails to the productList
-  productOne.showName()
-  productOne.showProductDetail()
-  console.log(productOne)
-  console.log(productOne.productList)
-  
-  const productTwo = new product('headphone', '111', 'EUR', '234')
-  productTwo.showName()
-  productTwo.showProductDetail()
-  console.log(productTwo)
-  console.log(productTwo.productList)
   
   
   
